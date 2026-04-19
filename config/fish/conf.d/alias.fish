80be365274009f6cd1673edf43f14483821ab2fc
OrzMiku <miku@ecy.pink>
2026-04-20 00:32:26 +0800

    update

--- config/fish/conf.d/alias.fish
+++ config/fish/conf.d/alias.fish
@@ -1,8 +1,3 @@
-if status is-interactive
-  alias ls='ls --color=auto'
-  alias grep='grep --color=auto'
-  alias rm='trash-put'
-  alias tl='trash-list'
-  alias tr='trash-restore'
-  alias zed='zeditor'
-end
+alias rm='trash-put'
+alias tl='trash-list'
+alias tr='trash-restore'
--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,4 +1,2 @@
 function fish_greeting
-  cowsay -f hellokitty "哈基米哦南北绿豆~"
 end
-
