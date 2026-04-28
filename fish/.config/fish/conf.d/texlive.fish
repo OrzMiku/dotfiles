2e662934e101f9c42bfd791b20372e1454631a78
OrzMiku <miku@ecy.pink>
2026-04-28 23:55:09 +0800

    conf(fish): fix manpath

--- fish/.config/fish/conf.d/texlive.fish
+++ fish/.config/fish/conf.d/texlive.fish
@@ -1,3 +1,3 @@
-set -Ux MANPATH /usr/local/texlive/2026/texmf-dist/doc/man:$MANPATH
-set -Ux INFOPATH /usr/local/texlive/2026/texmf-dist/doc/info:$INFOPATH
+set -Ux MANPATH /usr/local/texlive/2026/texmf-dist/doc/man $MANPATH
+set -Ux INFOPATH /usr/local/texlive/2026/texmf-dist/doc/info $INFOPATH
 fish_add_path /usr/local/texlive/2026/bin/x86_64-linux
