be3ac34388e1f2a8901fd1781b558256c19235d0
OrzMiku <miku@ecy.pink>
2026-07-06 15:42:12 +0800

    Create Microsoft.PowerShell_profile.ps1

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+$starshipCachePath = "$env:LOCALAPPDATA\starship_init.ps1"
+if (-not (Test-Path $starshipCachePath) -or (Get-Command starship -ErrorAction SilentlyContinue).Source -gt (Get-Item $starshipCachePath -ErrorAction SilentlyContinue).LastWriteTime) {
+    & starship init powershell | Out-File -FilePath $starshipCachePath -Encoding utf8
+}
+. $starshipCachePath
+
+Set-PSReadLineOption -EditMode Emacs
+
+Import-Module PSFzf -ErrorAction SilentlyContinue
+if (-not $?) {
+    Write-Host "Install PSFzf..." -ForegroundColor Cyan
+    Install-Module -Name PSFzf -Scope CurrentUser -Force -SkipPublisherCheck
+    Import-Module PSFzf
+}
+
+Set-PSFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSFzfOption -TabExpansion
+Set-PSFzfOption -TabCompletionPreviewWindow 'hidden|right|down|hidden'
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
