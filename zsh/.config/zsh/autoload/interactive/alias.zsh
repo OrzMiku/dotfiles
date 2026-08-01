acccd9feb3c26ad1af9929543f6b62c8750bd632
OrzMiku <miku@ecy.pink>
2026-08-01 23:46:02 +0800

    fix(zsh): 修复 trash-cli alias

--- zsh/.config/zsh/autoload/interactive/alias.zsh
+++ zsh/.config/zsh/autoload/interactive/alias.zsh
@@ -2,7 +2,7 @@ alias ll='ls -l'
 alias la='ls -la'
 
 # quoted subscript: shfmt's zsh parser misreads the hyphen as subtraction
-if (($+commands['trash-put'])); then
+if [[ -v commands['trash-put'] ]]; then
   alias rm='trash-put'
   alias tl='trash-list'
   alias tr='trash-restore'
