c392a797ab41d2606ea8d8115e1518432fa6fe9c
OrzMiku <miku@ecy.pink>
2026-09-11 01:25:40 +0800

    npm global prefix

--- zsh/.config/zsh/autoload/env/fnm.zsh
+++ zsh/.config/zsh/autoload/env/fnm.zsh
@@ -1,6 +1,8 @@
 fnm_dir="$HOME/.local/share/fnm"
+npm_prefix="$HOME/.local/npm"
 if [[ -x $fnm_dir/fnm ]]; then
   path=($fnm_dir $path)
   eval "$(fnm env --shell zsh)"
+  path=("$npm_prefix/bin" $path)
 fi
 unset fnm_dir
--- zsh/.config/zsh/autoload/interactive/alias.zsh
+++ zsh/.config/zsh/autoload/interactive/alias.zsh
@@ -1,3 +1,4 @@
+alias ls='ls --color'
 alias ll='ls -l'
 alias la='ls -la'
 
