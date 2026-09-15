d0232171f75f7bc1689ecdc0f047b55c20004eef
OrzMiku <miku@ecy.pink>
2026-09-15 16:27:54 +0800

    fnm use on cd

--- dot_config/zsh/autoload/env/fnm.zsh
+++ dot_config/zsh/autoload/env/fnm.zsh
@@ -2,7 +2,7 @@ fnm_dir="$HOME/.local/share/fnm"
 npm_prefix="$HOME/.local/npm"
 if [[ -x $fnm_dir/fnm ]]; then
   path=($fnm_dir $path)
-  eval "$(fnm env --shell zsh)"
+  eval "$(fnm env --use-on-cd --shell zsh)"
   path=("$npm_prefix/bin" $path)
 fi
 unset fnm_dir
